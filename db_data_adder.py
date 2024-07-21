47d0f98d1cdcb4101e70698577ad6f07c01b158b
RonanRibouletPython <newsletterpython13@gmail.com>
2024-07-21 18:43:55 +0200

    Successfully added summoners data to the Summoners MySQL DB

--- db_data_adder.py
+++ db_data_adder.py
@@ -1,9 +1,12 @@
 import mysql.connector as sql
 import logging
+import json
 from logging_setup import setup_logging
 
-setup_logging(log_file="logs/db_connection.log", level=logging.DEBUG)
+# Logging setup
+setup_logging(log_file="logs/db_data_adder.log", level=logging.DEBUG)
 
+# SQL connection to the lol_player_stats DB
 try:
     lol_db = sql.connect(
         host="localhost",
@@ -12,12 +15,88 @@ try:
         database="lol_player_stats"
     )
 
-    
+
+    logging.debug("Connection successful!")
 
 except sql.Error as err:
     logging.error(f"Error connecting to database: {err}")
 
-finally:
-    if sql.is_connected():
-        sql.close()
-        print("Connection closed.")
+
+
+# Extract the JSON data for the Summoners table of the DB
+def extract_summoners_data_from_json(file_path: str) -> dict: 
+    # Open the JSON file
+    try:
+        with open(file_path, "r") as json_file:
+            summoner_data = json.load(json_file)
+            logging.info("JSON file opened successfully.")
+    except FileNotFoundError:
+        logging.error(f"File {file_path} not found.")
+        raise
+    except json.JSONDecodeError:
+        logging.error(f"Invalid JSON format in file: {file_path}")
+        raise
+
+    info_query = (
+        summoner_data['id'], 
+        summoner_data["summoner_id"], 
+        summoner_data['region'], 
+        summoner_data['account_id'], 
+        summoner_data['puuid'],
+        summoner_data['game_name'],
+        summoner_data['tagline'],
+        summoner_data['name'],
+        summoner_data['internal_name'],
+        summoner_data['profile_image_url'],
+        summoner_data['level'],
+        summoner_data['updated_at'],
+        summoner_data['renewable_at'],
+    )
+
+    return info_query
+
+# Construct query for the MySQL DB
+def insert_data_summoners_table(file_path: str)-> str:
+
+    summoners_data = extract_summoners_data_from_json(file_path)
+
+    cursor = lol_db.cursor()
+
+    # Construct the query
+    query = """
+        INSERT INTO Summoners (
+            id,
+            summoner_id, 
+            region, 
+            account_id, 
+            puuid, 
+            game_name, 
+            tagline, 
+            name_,
+            internal_name, 
+            profile_icon_url, 
+            level, 
+            updated_at, 
+            renewable_at)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+    """
+    
+    # Execute the query
+    try:
+        cursor.execute(query, summoners_data)
+        lol_db.commit()
+        logging.info("Data inserted into Summoners table successfully.")
+    except sql.Error as err:
+        logging.error(f"Error inserting data into Summoners table: {err}")
+        lol_db.rollback()
+
+    return(cursor.rowcount, "record inserted.")
+
+
+if __name__ == "__main__":
+    file_path = "summoner_data_souvenir#2310.json"
+    insert_data_summoners_table(file_path)
+
+
+
+
--- league_scraper_opgg.py
+++ league_scraper_opgg.py
@@ -3,8 +3,14 @@ from opgg.params import Region
 
 import json
 from typing import Dict
+import logging
 
-from utils import extract_summoner_data as ext
+from utils import extract_summoner_data
+
+from logging_setup import setup_logging
+
+# Logging setup
+setup_logging(log_file="logs/league_scraper_opgg.log", level=logging.DEBUG)
 
 # Parameters
 summoner_name = "souvenir#2310"  
@@ -13,8 +19,12 @@ region = Region.EUW
 
 def save_to_json(data: Dict, filename: str = f"summoner_data_{summoner_name}.json"):
     """Saves the given data to a JSON file."""
-    with open(filename, "w") as f:
-        json.dump(data, f, indent=4)
+    try:
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
+    except Exception as e:
+        logging.error(f"Error saving data to JSON: {e}")
+
 
 
 def run_fetching(summoner_name: str, region: Region):
@@ -22,13 +32,20 @@ def run_fetching(summoner_name: str, region: Region):
     opgg = OPGG()
     summoner = opgg.search(summoner_name, region)
     
-    if summoner:
-        summoner_data = ext(summoner)
-        save_to_json(summoner_data)
-        print(f"Summoner data for {summoner_name} saved to summoner_data.json")
-    else:
-        print(f"Summoner {summoner_name} not found.")
+    # Adding the region to the JSON file 
+    temp = {
+        "region" : region
+    }
 
+    if summoner:
+        try: 
+            summoner_data = extract_summoner_data(summoner)
+            summoner_data.update(temp)
+            save_to_json(summoner_data)
+
+            logging.debug(f"Summoner data for {summoner_name} saved to summoner_data.json")
+        except Exception as e:
+            logging.debug(f"Summoner {summoner_name} not found throwing error: {e}")
 
 if __name__ == "__main__":
     run_fetching(summoner_name, region)
